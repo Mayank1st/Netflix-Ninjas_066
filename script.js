fc56efdfc7fa7793b1c91102a44e736fcf2948e0
Mayank Kumar <mayankdas99@gmail.com>
2024-06-12 23:03:32 +0530

    intial_push_updated_12062024

---
+++ script.js
@@ -0,0 +1,108 @@
+/* Navbar section start */
+
+
+
+
+/* Navbar section ends */
+
+
+
+
+/* Expand your horizons section start */
+
+
+
+/* Expand your horizons section ends */
+
+
+
+
+/* Section1(We collaborate with ) section start */
+
+
+
+
+/* Section1(We collaborate with ) section ends */
+
+
+
+
+/* Section-2(Launch a new career) section start */
+
+
+
+
+/* Section-2(Launch a new career) section ends */
+
+
+
+/* section-3(Degree Programs) section start */
+
+
+
+/* section-3(Degree Programs) section ends */
+
+
+
+
+/* section-4(Courses and Professional Certificates) section start */
+
+
+
+/* section-4(Courses and Professional Certificates) section ends */
+
+
+
+
+/* section-5(100% Free) section start */
+
+
+
+/* section-5(100% Free) section ends */
+
+
+
+/* Section-6(Explore Coursera)  section start */
+
+
+
+/* Section-6(Explore Coursera)  section ends */
+
+
+
+
+/* section-7(World-class learning)  section start */
+
+
+
+/* section-7(World-class learning)  section ends */
+
+
+/* section-8(From the Coursera community)  section start */
+
+
+/* section-8(From the Coursera community)  section ends */
+
+
+
+
+
+
+/* section-9(The ideal solution for your business)  section start */
+
+
+/* section-9(The ideal solution for your business)  section ends */
+
+
+
+/* section-10(Take the next step)  section start */
+
+
+/* section-10(Take the next step)  section ends */
+
+
+
+/* section-11(Footer)  section start */
+
+
+/* section-11(Footer)  section ends */
